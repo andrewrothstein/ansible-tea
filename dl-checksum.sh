59de1abb689794bec8b04ceae1780edde6a3271b
Andrew Rothstein <andrew.rothstein@gmail.com>
2025-11-30 20:35:24 -0500

    tea:0.11.1

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -10,12 +10,16 @@ dl()
     local arch=$3
     local dotexe=${4:-""}
     local platform="${os}-${arch}"
-    local file="tea-${ver}-${platform}${dotexe}.sha256"
+    local file="tea-${ver}-${platform}${dotexe}"
     local dlfile=$DIR/$file
-    # https://gitea.com/gitea/tea/releases/download/v0.9.2/tea-0.9.2-darwin-amd64.sha256
+    # https://gitea.com/gitea/tea/releases/download/v0.9.2/tea-0.9.2-darwin-amd64
     local url=$MIRROR/v${ver}/$file
+    if [ ! -e $dlfile ];
+    then
+        curl -sSLf -o $dlfile $url
+    fi
     printf "    # %s\n" $url
-    printf "    %s: sha256:%s\n" $platform $(curl -sSLf $url | awk '{ print $1}')
+    printf "    %s: sha256:%s\n" $platform $(sha256sum $dlfile | awk '{ print $1 }')
 }
 
 dl_ver ()
@@ -24,12 +28,12 @@ dl_ver ()
     printf "  '%s':\n" $ver
     dl $ver darwin amd64
     dl $ver darwin arm64
-    dl $ver linux 386
     dl $ver linux amd64
-    dl $ver linux arm
+    dl $ver linux arm-5
+    dl $ver linux arm-6
+    dl $ver linux arm-7
     dl $ver linux arm64
-    dl $ver windows 386 .exe
     dl $ver windows amd64 .exe
 }
 
-dl_ver ${1:-0.9.2}
+dl_ver ${1:-0.11.1}
